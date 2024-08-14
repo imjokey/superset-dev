2f3302714c719ad5b831823984fc7de4cbe6ab1f
jokey <me@jokey.li>
2024-08-14 23:23:59 +0800

    dev compose

--- docker/docker-front-dev.sh
+++ docker/docker-front-dev.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 
 set -e
 
@@ -6,4 +7,4 @@ cd /app/superset-frontend
 npm ci
 
 echo "Running frontend"
-npm run dev-server -- --port 8088 --env=--superset="${PROXY_ADDRESS:-http://host.docker.internal:8000}"
+npm run dev-server -- --port 8088 --env=--superset="${PROXY_ADDRESS:-http://superset_dev_app:${SUPERSET_PORT:-8000}}"
