735dcffaf523487ca226f257216eae79dd6cbd20
jokey <me@jokey.li>
2024-08-17 08:58:45 +0800

    up

--- build.sh
+++ build.sh
@@ -1,10 +1,12 @@
 #! /bin/sh
 
-# git clone --depth 1 --branch 4.0.2  https://github.com/apache/superset.git
 # docker build -t superset-4.0.2 .
 
 #docker-compose -f docker-dev.yaml up -d
 
 #docker-compose -f docker-dev.yaml start 
 #docker-compose -f docker-dev.yaml stop
+
+# start front only
+#PROXY_ADDRESS=http://49.51.178.63:8000 docker-compose  -f docker-dev.yaml up -d --no-deps superset-node
 
